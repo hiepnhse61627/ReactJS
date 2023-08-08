70988708623d9963e29f99f8e3d02351db1fea6f
HiepHoangNguyen <hiephoang.nguyen.hcm@gmail.com>
2023-08-08 22:34:35 +0700

    React Professional Project

---
+++ 16-fast-react-pizza/src/App.jsx
@@ -0,0 +1,55 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import Home from './ui/Home';
+import Error from './ui/Error';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import CreateOrder, {
+  action as createOrderAction,
+} from './features/order/CreateOrder';
+import Order, { loader as orderLoader } from './features/order/Order';
+import { action as updateOrderAction } from './features/order/UpdateOrder';
+import AppLayout from './ui/AppLayout';
+
+// In the new React Router (6.4), if we want to use data loaders, data actions, data fetchers, we need to use the following syntax
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/menu',
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+      {
+        path: '/order/new',
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
+      {
+        path: '/order/:orderId',
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+        action: updateOrderAction,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
