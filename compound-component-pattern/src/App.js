a785c2c770f927c4af591300470059e173c5e439
HiepHoangNguyen <hiephoang.nguyen.hcm@gmail.com>
2023-09-04 11:57:27 +0700

    Compound Component Pattern

---
+++ compound-component-pattern/src/App.js
@@ -0,0 +1,33 @@
+import Counter from "./Counter";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Compound Component Pattern</h1>
+      {/* <Counter
+        iconIncrease="+"
+        iconDecrease="-"
+        label="My NOT so flexible counter"
+        hideLabel={false}
+        hideIncrease={false}
+        hideDecrease={false}
+      /> */}
+
+      <Counter>
+        <Counter.Label>My super flexible counter</Counter.Label>
+        <Counter.Increase icon="+" />
+        <Counter.Decrease icon="-" />
+        <Counter.Count />
+      </Counter>
+
+      <div>
+        <Counter>
+          <Counter.Decrease icon="◀" />
+          <Counter.Count />
+          <Counter.Increase icon="▶" />
+        </Counter>
+      </div>
+    </div>
+  );
+}
